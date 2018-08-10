69afbeb38c25c85f42575d7c4b899a3037f1157e
David Alvaro Gray Velasco <dvelasco@student.neumont.edu>
2018-08-10 09:44:27 -0600

    Finished Abstract Piece Class

--- Chess/Chess/Models/Piece.cs
+++ Chess/Chess/Models/Piece.cs
@@ -8,6 +8,9 @@ namespace Chess.Models
 {
     public abstract class Piece
     {
+        private Int16 xPosition;
+        private Int16 yPosition;
 
+        public abstract void Move(String newPosition);
     }
 }
