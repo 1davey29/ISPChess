059f3fb4e7b6eb3dbb7ec9f98f49cedf9de8e41a
lukeshep42 <lukeshep42@hotmail.com>
2018-08-10 09:38:48 -0600

    created gameSpace with initialization

--- Chess/Chess/Models/Board.cs
+++ Chess/Chess/Models/Board.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Chess.Models
 {
-    class Board
+    public class Board
     {
+        public char[,] gameSpace { get; set; }
+
+        public Board()
+        {
+            gameSpace = new char[8, 8];
+            for (int x = 0; x < gameSpace.Length; x++)
+            {
+                for (int y = 0; y < gameSpace.Length; y++)
+                {
+                    gameSpace[x, y] = '-';
+                }
+            }
+        }
     }
 }
