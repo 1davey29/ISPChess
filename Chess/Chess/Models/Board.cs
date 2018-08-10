82a8f748466f6a648e0ba8759be8353dc2fd53bc
lukeshep42 <lukeshep42@hotmail.com>
2018-08-10 10:12:56 -0600

    added DisplayBoard method to Board class

--- Chess/Chess/Models/Board.cs
+++ Chess/Chess/Models/Board.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chess.Models.Pieces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,11 @@ namespace Chess.Models
                     gameSpace[x, y] = new EmptyPiece();
                 }
             }
+        }
+
+        public void DisplayBoard()
+        {
+
         }
     }
 }
