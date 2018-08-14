16a0eb790704d4c595d95145ca3983f571baf3c7
lukeshep42 <lukeshep42@hotmail.com>
2018-08-14 10:51:19 -0600

    implemented move validation for pawn

--- Chess/Chess/Models/Pieces/Pawn.cs
+++ Chess/Chess/Models/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chess.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,35 @@ namespace Chess.Models.Pieces
 {
     public class Pawn : Piece
     {
-        public override void Move(string newPosition)
+        private bool hasMoved = false;
+        public override int Move(string newPosition)
         {
-            throw new NotImplementedException();
+            int[] positionXY;
+
+            positionXY = ChessController.ConvertToXY(newPosition);
+
+            int distanceX = Math.Abs(XPosition - positionXY[0]);
+            int distanceY = Math.Abs(YPosition - positionXY[1]);
+
+            if ((positionXY[0] < 0 || positionXY[0] > 7) || (positionXY[1] < 0 || positionXY[1] > 7))
+            {
+                throw new ArgumentOutOfRangeException("The position specified is out of bounds!");
+            }
+
+            if (distanceX != 0 && ((!hasMoved && distanceY > 2) || distanceY > 1))
+            {
+                throw new ArgumentException("Invalid move for the specified piece");
+            }
+
+            ChessController.Board.gameSpace[positionXY[0], positionXY[1]] = this;
+            ChessController.Board.gameSpace[XPosition, YPosition] = new EmptyPiece(XPosition, YPosition);
+
+            XPosition = positionXY[0];
+            YPosition = positionXY[1];
+
+            hasMoved = true;
+
+            return 0;
         }
 
         public Pawn(String color) : base(color.Equals("White") ? 'p' : 'P')
