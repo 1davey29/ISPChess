c6ba218dfb91d4f69a6f7b9df080a28911d330f5
lukeshep42 <lukeshep42@hotmail.com>
2018-08-17 17:41:08 -0600

    finished pawn move validation

--- Chess/Chess/Models/Pieces/Pawn.cs
+++ Chess/Chess/Models/Pieces/Pawn.cs
@@ -24,10 +24,54 @@ namespace Chess.Models.Pieces
                 throw new ArgumentOutOfRangeException("The position specified is out of bounds!");
             }
 
-            if (distanceX != 0 && ((!hasMoved && distanceY > 2) || distanceY > 1))
+            if ((distanceX != 0 && ((!hasMoved && distanceY > 2) || distanceY > 1)) ^ (distanceX != 1 && distanceY != 1))
             {
                 throw new ArgumentException("Invalid move for the specified piece");
             }
+
+            if (ChessController.Board.gameSpace[positionXY[0], positionXY[1]].GetType() != typeof(EmptyPiece))
+            {
+                if (distanceX == 1 && distanceY == 1)
+                {
+                    if (!(Char.IsLower(this.GetSymbol()) ?
+                        Char.IsUpper(ChessController.Board.gameSpace[positionXY[0], positionXY[1]].GetSymbol()) :
+                        Char.IsLower(ChessController.Board.gameSpace[positionXY[0], positionXY[1]].GetSymbol())))
+                    {
+                        throw new Exception("You cannot take your own piece");
+                    }
+
+                    ChessController.Board.gameSpace[positionXY[0], positionXY[1]] = this;
+                    ChessController.Board.gameSpace[XPosition, YPosition] = new EmptyPiece(XPosition, YPosition);
+
+                    XPosition = positionXY[0];
+                    YPosition = positionXY[1];
+
+                }
+                else
+                {
+                    throw new Exception("Invalid move");
+                }
+            }
+
+            if (distanceY == 2)
+            {
+                for (int y = (YPosition - positionXY[1] > 0) ? 
+                    positionXY[1] : distanceY; (YPosition - positionXY[1] > 0) ? y <= distanceY : 
+                    y >= positionXY[1];)
+                {
+
+                    if (ChessController.Board.gameSpace[positionXY[0], y].GetType() != typeof(EmptyPiece))
+                    {
+                        throw new Exception("Cannot move through pieces");
+                    }
+
+                    if (YPosition - positionXY[1] > 0)
+                        y++;
+                    else
+                        y--;
+                }
+            } 
+
 
             ChessController.Board.gameSpace[positionXY[0], positionXY[1]] = this;
             ChessController.Board.gameSpace[XPosition, YPosition] = new EmptyPiece(XPosition, YPosition);
