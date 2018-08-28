bc12dc17c375ea56585ddf1b8a219c26f429eb60
David Alvaro Gray Velasco <dvelasco@student.neumont.edu>
2018-08-28 09:31:27 -0600

    Fix

--- Chess/Chess/Models/Pieces/Pawn.cs
+++ Chess/Chess/Models/Pieces/Pawn.cs
@@ -88,6 +88,81 @@ namespace Chess.Models.Pieces
             return 0;
         }
 
+        public override int Move(int[] positionXY)
+        {
+            int distanceX = Math.Abs(XPosition - positionXY[0]);
+            int distanceY = Math.Abs(YPosition - positionXY[1]);
+
+            if ((positionXY[0] < 0 || positionXY[0] > 7) || (positionXY[1] < 0 || positionXY[1] > 7))
+            {
+                return 4;
+            }
+
+            if (distanceX != 0 || ((hasMoved) ? distanceY > 1 : distanceY > 2))
+            {
+                if (distanceX != 1 || distanceY != 1)
+                {
+                    return 1;
+                }
+            }
+
+            if (ChessController.Board.gameSpace[positionXY[0], positionXY[1]].GetType() != typeof(EmptyPiece))
+            {
+                if (distanceX == 1 && distanceY == 1)
+                {
+                    if (IsSameColor(positionXY))
+                    {
+                        return 2;
+                    }
+                    if (ChessController.Board.gameSpace[positionXY[0], positionXY[1]].GetType() == typeof(King))
+                    {
+                        return 5;
+                    }
+                }
+                else
+                {
+                    if (IsSameColor(positionXY))
+                    {
+                        return 2;
+                    }
+                    return 3;
+                }
+            }
+            else if (distanceX == 1 && distanceY == 1)
+            {
+                return 1;
+            }
+
+            if (distanceY == 2)
+            {
+                for (int y = (YPosition - positionXY[1] > 0) ?
+                    positionXY[1] : distanceY; (YPosition - positionXY[1] > 0) ? y <= distanceY :
+                    y >= positionXY[1];)
+                {
+
+                    if (ChessController.Board.gameSpace[positionXY[0], y].GetType() != typeof(EmptyPiece))
+                    {
+                        if (IsSameColor(positionXY))
+                        {
+                            return 2;
+                        }
+                        return 3;
+                    }
+
+                    if (YPosition - positionXY[1] > 0)
+                        y++;
+                    else
+                        y--;
+                }
+            }
+
+            UpdateBoard(positionXY);
+
+            hasMoved = true;
+
+            return 0;
+        }
+
         public Pawn(String color, int x, int y) : base(color.Equals("White") ? 'p' : 'P', x, y)
         {
         }
