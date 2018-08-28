ddb582f0403d47b08ac935d261d9b52c9e534459
David Alvaro Gray Velasco <dvelasco@student.neumont.edu>
2018-08-28 10:55:05 -0600

    3 != 5

--- Chess/Chess/Models/Pieces/Rook.cs
+++ Chess/Chess/Models/Pieces/Rook.cs
@@ -185,7 +185,7 @@ namespace Chess.Models.Pieces
 
                                 if (Char.IsUpper(ChessController.Board.gameSpace[changedPos + i, piece.YPosition].GetSymbol()) ^ Char.IsUpper(piece.GetSymbol()))
                                 {
-                                    return 3;
+                                    return 5;
                                 }
                                 else
                                 {
@@ -283,7 +283,7 @@ namespace Chess.Models.Pieces
 
                                 if (Char.IsUpper(ChessController.Board.gameSpace[piece.XPosition, changedPos + i].GetSymbol()) ^ Char.IsUpper(piece.GetSymbol()))
                                 {
-                                    return 3;
+                                    return 5;
                                 }
                                 else
                                 {
